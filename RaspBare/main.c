e2b6ccd5f34c2b64339b000fc9f2cdd31820d03e
WhiteRiceMGMG <3223mq118@gmail.com>
2025-06-27 00:04:14 +0900

    a

---
+++ RaspBare/main.c
@@ -0,0 +1,25 @@
+#include <stdint.h>
+
+#define GPIO_BASE     0x40014000
+#define GPIO_OE       (*(volatile uint32_t *)(GPIO_BASE + 0x20)) // Output enable
+#define GPIO_OUT      (*(volatile uint32_t *)(GPIO_BASE + 0x10)) // GPIO out
+#define LED_PIN       15
+
+void delay(volatile uint32_t t) {
+    while (t--) __asm__("nop");
+}
+
+int main(void) {
+    // set pin 15 as output
+    GPIO_OE |= (1 << LED_PIN);
+
+    while (1) {
+        // turn on
+        GPIO_OUT |= (1 << LED_PIN);
+        delay(1000000);
+
+        // turn off
+        GPIO_OUT &= ~(1 << LED_PIN);
+        delay(1000000);
+    }
+}

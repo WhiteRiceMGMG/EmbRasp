30610cb403e8ac1310ce60ea7f8a410af71685f0
Ogawa-Rasp <3223mq118@gmail.com>
2025-06-29 11:54:35 +0900

    a :

--- RaspBare3/gpio.h
+++ RaspBare3/gpio.h
@@ -1,23 +1,21 @@
 #ifndef GPIO_H
- #define GPIO_H
- 
- #include <stdint.h>
- 
- #define GPIO_BASE 0x3F200000 // BCM2710A1用
-			      //  
-			      //   typedef struct {
-			      //        volatile uint32_t GPFSEL[6];
-			      //             volatile uint32_t RESERVED0;
-			      //                  volatile uint32_t GPSET[2];
-			      //                       volatile uint32_t RESERVED1;
-			      //                            volatile uint32_t GPCLR[2];
-			      //                                 // 他レジスタは省略
-			      //                                  } GPIORegisters;
-			      //                                   
-			      //                                    extern volatile GPIORegisters *gpio;
-			      //                                     
-			      //                                      void gpio_init();
-			      //                                       void gpio_set_output(int pin);
-			      //                                        void gpio_write(int pin, int value);
-			      //                                         
-			      //                                          #endif
+#define GPIO_H
+#include <stdint.h>
+#define GPIO_BASE 0x3f200000
+
+typedef struct {
+	volatile uint32_t GPFSEL[6];
+	volatile uint32_t RESERVED0;
+	volatile uint32_t GPSET[2];
+	volatile uint32_t RESERVED1;
+	volatile uint32_t GPCLR[2];
+
+} GPIORegisters;
+
+extern volatile GPIORegisters *gpio;
+
+void gpio_init();
+void gpio_set_output(int pin);
+void gpio_write(int pin, int value);
+
+#endif
--- RaspBare3/main.c
+++ RaspBare3/main.c
@@ -1,19 +1,16 @@
 #include "gpio.h"
- #include <unistd.h>
- 
- int main() {
-	      gpio_init();
-	       
-	           int pin = 17; // BCM GPIO17（物理ピン11）
-				 //  
-				 //       gpio_set_output(pin);
-				 //        
-				 //             while (1) {
-				 //                      gpio_write(pin, 1); // ON
-				 //                               sleep(1);
-				 //                                        gpio_write(pin, 0); // OFF
-				 //                                                 sleep(1);
-				 //                                                      }
-				 //                                                       
-				 //                                                            return 0;
-				 //                                                             }
+#include <unistd.h>
+
+int main(void) {
+	gpio_init();
+	int pin = 17;
+	gpio_set_output(pin);
+
+	while(1) {
+		gpio_write(pin, 1);
+		sleep(1);
+		gpio_write(pin, 0);
+		sleep(1);
+	}
+	return 0;
+}

05e85a0d5bec06d890da1bb91979381d723f0c3d
WhiteRiceMGMG <3223mq118@gmail.com>
2025-07-06 23:49:17 +0900

    a

--- rtos_study/lesson1/boot/reset_hdr.c
+++ rtos_study/lesson1/boot/reset_hdr.c
@@ -2,3 +2,11 @@
 #include <KedSysDef.h>
 #include <KedSysLib.h>
 #include <KedKernelDef.h>
+extern const void *_data_org;
+extern const void *_data_start;
+extern const void *_data_end;
+extern const void *_bss_start;
+extern const void *_bss_end;
+
+
+

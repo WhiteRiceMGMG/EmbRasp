da51006734c6c1e73947a0b51da55b1622179b1f
WhiteRiceMGMG <3223mq118@gmail.com>
2025-07-10 00:10:14 +0900

    a

--- RTOS/rtos_v3/kernel/syslib.c
+++ RTOS/rtos_v3/kernel/syslib.c
@@ -14,10 +14,12 @@ void tm_com_init(void)
     out_w(UART0_BASE+UARTx_FBRD, 52);
     out_w(UART0_BASE+UARTx_LCR_H, 0x70);                            /* データ形式設定 */
     out_w(UART0_BASE+UARTx_CR, UART_CR_RXE|UART_CR_TXE|UART_CR_EN); /* 通信イネーブル */
+    /*RXE受信有効，TXE送信有効，UARTモジュール全体を有効化*/
 }
 
 
 /* デバッグ用UART出力 */
+
 UINT tm_putstring(char* str)
 {
     UINT	cnt = 0;

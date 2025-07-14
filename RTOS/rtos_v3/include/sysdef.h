0a49e20caa55ad32f61cf64e138e3ccd689cf191
WhiteRiceMGMG <3223mq118@gmail.com>
2025-07-14 23:49:47 +0900

    a

--- RTOS/rtos_v3/include/sysdef.h
+++ RTOS/rtos_v3/include/sysdef.h
@@ -60,8 +60,8 @@
 /* Reset Controler */
 #define RESETS_BASE             0x4000C000                  /*リセットコントローラのベースアドレス*/
 #define RESETS_RESET            (RESETS_BASE+0x0)           /*各モジュールをリセットするためのレジスタ*/
-#define RESETS_WDSEL            (RESETS_BASE+0x4)           /**/
-#define RESETS_RESET_DONE       (RESETS_BASE+0x8)           /**/
+#define RESETS_WDSEL            (RESETS_BASE+0x4)           /*ウォッチドッグリセット時に影響を与えるモジュールを選ぶ*/
+#define RESETS_RESET_DONE       (RESETS_BASE+0x8)           /*リセットが完了したか確認するレジスタ*/
 
 #define RESETS_RESET_ADC        (0x00000001)
 #define RESETS_RESET_I2C0       (0x00000008)

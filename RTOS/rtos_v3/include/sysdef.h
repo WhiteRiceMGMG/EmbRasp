e70d08d2d452d96ddbc3e1754eb60df514b2f4eb
WhiteRiceMGMG <3223mq118@gmail.com>
2025-07-15 07:22:38 +0900

    a

--- RTOS/rtos_v3/include/sysdef.h
+++ RTOS/rtos_v3/include/sysdef.h
@@ -69,20 +69,20 @@
 
 /* GPIO */
 #define IO_BANK0_BASE           0x40014000                  /*GPIO制御レジスタのベースアドレス*/
-#define	GPIO_CTRL(n)            (IO_BANK0_BASE+0x04+(n*8))  /**/
+#define	GPIO_CTRL(n)            (IO_BANK0_BASE+0x04+(n*8))  /*GPIOピンn番目の制御用レジスタのアドレス*/
     
-#define	GPIO_CTRL_FUNCSEL_I2C   3                           /**/
-#define	GPIO_CTRL_FUNCSEL_NULL  31                          /**/
+#define	GPIO_CTRL_FUNCSEL_I2C   3                           /*GPIOをI2C機能として使うときに指定する．*/
+#define	GPIO_CTRL_FUNCSEL_NULL  31                          /*GPIO機器を無しにする番号*/
 
-#define PADS_BANK0_BASE         0x4001c000
-#define	GPIO(n)                 (PADS_BANK0_BASE+0x4+(n*4))
+#define PADS_BANK0_BASE         0x4001c000                  /**/
+#define	GPIO(n)                 (PADS_BANK0_BASE+0x4+(n*4)) /**/
 
-#define	GPIO_OD                 (1<<7)
-#define	GPIO_IE                 (1<<6)
-#define	GPIO_DRIVE_2MA          (0<<4)
-#define	GPIO_DRIVE_4MA          (1<<4)
-#define	GPIO_DRIVE_8MA          (2<<4)
-#define	GPIO_DRIVE_12MA         (3<<4)
+#define	GPIO_OD                 (1<<7)                      /**/
+#define	GPIO_IE                 (1<<6)                      /**/
+#define	GPIO_DRIVE_2MA          (0<<4)                      /**/
+#define	GPIO_DRIVE_4MA          (1<<4)                      /**/
+#define	GPIO_DRIVE_8MA          (2<<4)                      /**/
+#define	GPIO_DRIVE_12MA         (3<<4)                      /**/
 #define	GPIO_PUE                (1<<3)
 #define	GPIO_PDE                (1<<2)
 #define	GPIO_SHEMITT            (1<<1)

2e27405192aca42dc0a91ba31919c75b17fd3c75
WhiteRiceMGMG <3223mq118@gmail.com>
2025-07-15 20:23:21 +0900

    a

---
+++ minBare/Debug/subdir.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../main.c 
+
+OBJS += \
+./main.o 
+
+C_DEPS += \
+./main.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+%.o: ../%.c subdir.mk
+	@echo 'Building file: $<'
+	@echo 'Invoking: GNU Arm Cross C Compiler'
+	arm-none-eabi-gcc -mcpu=cortex-m0plus -mthumb -O0 -ffreestanding -g3 -I/Users/Hakumai/eclipse-workspace/minBare/includes -std=c99 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+

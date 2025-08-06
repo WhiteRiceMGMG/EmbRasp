b289987f6c55838c1a8ec85de3ad299d6ab63a3f
ogawa-vaio <3223mq118@gmail.com>
2025-08-06 22:32:41 +0900

    a

---
+++ min_ver1/sysdef.h
@@ -0,0 +1,5 @@
+#define IO_BANK0_GPIO25_CTRL (*(volatile unsigned int *)0x400140CC)
+#define SIO_GPIO_OE_SET      (*(volatile unsigned int *)0xD0000024)
+#define SIO_GPIO_OUT_SET     (*(volatile unsigned int *)0xD0000014)
+
+

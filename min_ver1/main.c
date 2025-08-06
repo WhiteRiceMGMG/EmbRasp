9f2890e9afaf069f16a756a2f29ac67287f466e5
ogawa-vaio <3223mq118@gmail.com>
2025-08-07 00:11:13 +0900

    a

--- min_ver1/main.c
+++ min_ver1/main.c
@@ -1,10 +1,15 @@
-int main(void)
-{
-    while(1)
-    {
-        IO_BANK0_GPIO25_CTRL = 5;         // GPIO25をSIO機能に設定
-        SIO_GPIO_OE_SET = (1 << 25);      // GPIO25を出力に設定
-        SIO_GPIO_OUT_SET = (1 << 25);     // GPIO25をHigh（LED点灯）
-    }
-    return 0;
+#define IO_BANK0_GPIO25_CTRL (*(volatile unsigned int *)0x400140CC)
+#define SIO_GPIO_OE_SET      (*(volatile unsigned int *)0xD0000024)
+#define SIO_GPIO_OUT_SET     (*(volatile unsigned int *)0xD0000014)
+
+void main(void) {
+    IO_BANK0_GPIO25_CTRL = 5;
+    SIO_GPIO_OE_SET = (1 << 25);
+    SIO_GPIO_OUT_SET = (1 << 25);
+    while(1);
+}
+
+void __attribute__((naked)) __reset_handler(void) {
+    main();
+    while(1);
 }

4263b3ec126c2a8e97aff0b800e93ff904160c3b
ogawa-vaio <3223mq118@gmail.com>
2025-08-06 23:00:47 +0900

    a

--- min_ver1/main.c
+++ min_ver1/main.c
@@ -1,3 +1,10 @@
-IO_BANK0_GPIO25_CTRL = 5;         // GPIO25をSIO機能に設定
-SIO_GPIO_OE_SET = (1 << 25);      // GPIO25を出力に設定
-SIO_GPIO_OUT_SET = (1 << 25);     // GPIO25をHigh（LED点灯）
+int main(void)
+{
+    while(1)
+    {
+        IO_BANK0_GPIO25_CTRL = 5;         // GPIO25をSIO機能に設定
+        SIO_GPIO_OE_SET = (1 << 25);      // GPIO25を出力に設定
+        SIO_GPIO_OUT_SET = (1 << 25);     // GPIO25をHigh（LED点灯）
+    }
+    return 0;
+}

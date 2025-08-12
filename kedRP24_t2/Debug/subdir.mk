e353318c9593f9e1dcc517192743335c27483c26
ogawa-vaio <3223mq118@gmail.com>
2025-08-12 17:36:56 +0900

    a

---
+++ kedRP24_t2/Debug/subdir.mk
@@ -0,0 +1,41 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../boot2.c \
+../main.c 
+
+S_UPPER_SRCS += \
+../startup.S 
+
+OBJS += \
+./boot2.o \
+./main.o \
+./startup.o 
+
+S_UPPER_DEPS += \
+./startup.d 
+
+C_DEPS += \
+./boot2.d \
+./main.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+%.o: ../%.c subdir.mk
+	@echo 'Building file: $<'
+	@echo 'Invoking: GNU Arm Cross C Compiler'
+	arm-none-eabi-gcc -mcpu=cortex-m0plus -mthumb -O0 -ffreestanding -g3 -std=c99 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+%.o: ../%.S subdir.mk
+	@echo 'Building file: $<'
+	@echo 'Invoking: GNU Arm Cross Assembler'
+	arm-none-eabi-gcc -mcpu=cortex-m0plus -mthumb -O0 -ffreestanding -g3 -x assembler-with-cpp -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+

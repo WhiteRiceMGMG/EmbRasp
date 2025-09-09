85747a7d6b7dd7667a1327a3ac39a75c058209da
ogawa-vaio <3223mq118@gmail.com>
2025-09-09 23:50:23 +0900

    a

--- RTOSv6/common/main.c
+++ RTOSv6/common/main.c
@@ -1,18 +1,11 @@
-/*
- * main.c
- *
- *  Created on: 2025/07/25
- *      Author: shion
- */
 #include <kedknl.h>
 
-/* 時間待ち関数 */
 static void delay_ms( UINT ms)
 {
     UINT cnt = ms/TIMER_PERIOD;
 
     while(cnt) {
-        if((in_w(SYST_CSR) & SYST_CSR_COUNTFLAG)!=0) {  /* TIMER_PERIOD経過するとフラグがセット */
+        if((in_w(SYST_CSR) & SYST_CSR_COUNTFLAG)!=0) {  //あとで抽象化したい．
             cnt--;
         }
     }
@@ -21,36 +14,38 @@ static void delay_ms( UINT ms)
 
 int main(void)
 {
-	u1 num = 0;
-    tm_com_init();                      /* デバッグ出力の初期化 */
-    tm_putstring("hello,world\n");      /* デバッグ出力 */
-
-    while(1) {
-        if (in_w(GPIO_IN) & (1 << 18)) {    // GPIO18が1ならボタン押されてる（プルアップ設定なら押してないときは1、押すと0なので反転判定に注意）
-            out_w(GPIO_OUT_SET, (1 << 15));  // LED ON
+    while(1)
+    {
+        if(Ked.IgSw.Push())  
+        {
+            Ked.FiftLed.On();
         } else {
-            out_w(GPIO_OUT_CLR, (1 << 15));  // LED OFF
+            Ked.FiftLed.Off();
         }
 
-        if (in_w(GPIO_IN) & (1 << 20)) {    // GPIO18が1ならボタン押されてる（プルアップ設定なら押してないときは1、押すと0なので反転判定に注意）
-            out_w(GPIO_OUT_SET, (1 << 13));  // LED ON
+        if(Ked.UpSw.Push())
+        {
+            Ked.ThirtLed.On();
         } else {
-            out_w(GPIO_OUT_CLR, (1 << 13));  // LED OFF
+            Ked.ThirtLed.Off();
         }
 
-        if (in_w(GPIO_IN) & (1 << 16)) {    // GPIO18が1ならボタン押されてる（プルアップ設定なら押してないときは1、押すと0なので反転判定に注意）
-            out_w(GPIO_OUT_SET, (1 << 11));  // LED ON
+        if(Ked.DwSw.Push())
+        {
+            Ked.ElevLed.On();
         } else {
-            out_w(GPIO_OUT_CLR, (1 << 11));  // LED OFF
+            Ked.ElevLed.Off();
         }
 
-        if (in_w(GPIO_IN) & (1 << 22)) {    // GPIO18が1ならボタン押されてる（プルアップ設定なら押してないときは1、押すと0なので反転判定に注意）
-            out_w(GPIO_OUT_SET, (1 << 9));  // LED ON
+        if(Ked.ConfSw.Push())
+        {
+            Ked.NineLed.On();
         } else {
-            out_w(GPIO_OUT_CLR, (1 << 9));  // LED OFF
+            Ked.NineLed.Off();
         }
 
-        delay_ms(50);  // ボタンのチャタリング防止とCPU負荷軽減
+        delay_ms(50);
+
     }
     return 0;
 }

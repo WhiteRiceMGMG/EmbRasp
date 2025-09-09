8325029e0f72517a9814a5988332f3206b193c92
ogawa-vaio <3223mq118@gmail.com>
2025-09-09 23:10:33 +0900

    a

--- RTOSv6/include/button.c
+++ RTOSv6/include/button.c
@@ -1,11 +1,32 @@
+/**********************************
+イグニッションオン/オフスイッチ
+ドライブモードセレクトアップスイッチ
+ドライブモードセレクトダウンスイッチ
+アクチュエータ稼働確認用スイッチ
+***********************************/
+
 #include "Button.h"
 #include "kedknl.h"
 
-int Button_On(void)
+u2 u2g_btnmodl_igswonoff(void)
+{
+    return (in_w(GPIO_IN) & (1 << 16)) != 0;
+}
+
+u2 u2g_btnmodl_drvmdmsswup(void)
 {
     return (in_w(GPIO_IN) & (1 << 18)) != 0;
 }
 
+u2 u2g_btnmodl_drvmdmsswdw(void)
+{
+    return (in_w(GPIO_IN) & ( 1 << 20)) != 0;
+}
+
+u2 u2g_btnmodl_confacsw(void)
+{
+    return (in_w(GPIO_IN) & (1 << 22)) != 0;
+}
 
 
 

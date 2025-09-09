1e856e3bb1e1b84ecf3c5684fb32d431bbd45f1f
ogawa-vaio <3223mq118@gmail.com>
2025-09-09 23:25:41 +0900

    a

--- RTOSv6/include/led.c
+++ RTOSv6/include/led.c
@@ -2,32 +2,41 @@
 
 void vdg_ledmodl_fiftledon(void)
 {
+    out_w(GPIO_OUT_SET, (1 << 15));
+}
 
+void vdg_ledmodl_fiftledoff(void)
+{
+    out_w(GPIO_OUT_CLR, (1 << 15));
 }
 
 void vdg_ledmodl_thirtledon(void)
 {
+    out_w(GPIO_OUT_SET, (1 << 13));
+}
 
+void vdg_ledmodl_thirtledoff(void)
+{
+    out_w(GPIO_OUT_CLR, (1 << 13));
 }
 
 void vdg_ledmodl_elevledon(void)
 {
+    out_w(GPIO_OUT_SET, (1 << 11));
+}
 
+void vdg_ledmodl_elevledoff(void)
+{
+    out_w(GPIO_OUT_CLR, (1 << 11));
 }
 
 void vdg_ledmodl_nineledon(void)
 {
-
+    out_w(GPIO_OUT_SET, (1 << 9));
 }
 
-15 13 11 9
-
-void Led_On(void)
+void vdg_ledmodl_nineledon(void)
 {
-	out_w(GPIO_OUT_SET, (1 << 17));
+    out_w(GPIO_OUT_CLR, (1 << 9));
 }
 
-void Led_Off(void)
-{
-	out_w(GPIO_OUT_CLR, (1 << 17));
-}

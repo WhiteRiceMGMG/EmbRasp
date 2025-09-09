1f70e09f9e1915f013708ae3bda616d45067dd9e
ogawa-vaio <3223mq118@gmail.com>
2025-09-09 23:14:07 +0900

    a

--- RTOSv6/include/button.h
+++ RTOSv6/include/button.h
@@ -1,10 +1,15 @@
 #ifndef BUTTON_H
 #define BUTTON_H
 #include "kedknl.h"
+
 typedef struct
 {
-    int (*On)(void);
+    u2 (*Push)(void);
 } ButtonDriver;
 
-int Button_On(void);
+u2 u2g_btnmodl_igswonoff(void)
+u2 u2g_btnmodl_drvmdmsswup(void);
+u2 u2g_btnmodl_drvmdmsswdw(void);
+u2 u2g_btnmodl_confacsw(void);
+
 #endif

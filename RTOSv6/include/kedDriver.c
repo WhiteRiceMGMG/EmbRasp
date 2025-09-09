7df1cbba433c44a96e292c6f88193831d799605e
ogawa-vaio <3223mq118@gmail.com>
2025-09-09 23:37:58 +0900

    a

--- RTOSv6/include/kedDriver.c
+++ RTOSv6/include/kedDriver.c
@@ -8,15 +8,41 @@
 #include "KedDriver.h"
 
 KedDriver Ked =
+{
+    .IgSw = 
     {
-        .Led =
-        {
-            .On  = Led_On,
-			.Off = Led_Off
-        },
-
-        .Button =
-        {
-        	.On  = Button_On
-        }
-    };
+        .Push = u2g_btnmodl_igswonoff 
+    },
+    .UpSw =
+    {
+        .Push = u2g_btnmodl_drvmdmsswup
+    },
+    .DwSw =
+    {
+        .Push = u2g_btnmodl_drvmdmsswdw
+    }
+    .ConfSw =
+    {
+        .Push = u2g_btnmodl_confacsw
+    }
+    .FiftLed = 
+    {
+        .On  = vdg_ledmodl_fiftledon,
+        .Off = vdg_ledmodl_fiftledoff
+    },
+    .ThirtLed = 
+    {
+        .On  = vdg_ledmodl_thirtledon,
+        .Off = vdg_ledmodl_thirtledoff
+    },
+    .ElevLed =
+    {
+        .On  = vdg_ledmodl_elevledon,
+        .Off = vdg_ledmodl_elevledoff
+    },
+    .NineLed =
+    {
+        .On  = vdg_ledmodl_nineledon,
+        .Off = vdg_ledmodl_nineledoff
+    }
+};

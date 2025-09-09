acebd66d7dde3c659a091d9b314ed4033dd84af7
ogawa-vaio <3223mq118@gmail.com>
2025-09-09 23:26:21 +0900

    a

--- RTOSv6/include/led.h
+++ RTOSv6/include/led.h
@@ -8,8 +8,12 @@ typedef struct
 } LedDriver;
 
 void vdg_ledmodl_fiftledon(void);
+void vdg_ledmodl_fiftledoff(void);
 void vdg_ledmodl_thirtledon(void);
+void vdg_ledmodl_thirtledoff(void);
 void vdg_ledmodl_elevledon(void);
+void vdg_ledmodl_elevledoff(void);
 void vdg_ledmodl_nineledon(void);
+void vdg_ledmodl_nineledoff(void);
 
 #endif

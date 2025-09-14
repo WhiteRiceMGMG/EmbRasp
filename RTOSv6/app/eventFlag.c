2bc92e7140920fe08c95002b70106f41c09a64ed
ogawa-vaio <3223mq118@gmail.com>
2025-09-15 02:19:17 +0900

    a

--- RTOSv6/app/eventFlag.c
+++ RTOSv6/app/eventFlag.c
@@ -2,5 +2,12 @@
 /*イベントフラグ設定ファイル                    */
 /******************************************** */
 
+#include "eventFlag.h"
 
+static EventFlag_t eventFlag;
+
+void initialEventFlag(void)
+{
+    eventFlag.flags = 0;
+}
 

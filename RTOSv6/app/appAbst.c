f5251b94fd6e285ef32b977c9bc3f984b8bdf986
ogawa-vaio <3223mq118@gmail.com>
2025-09-15 02:49:34 +0900

    a

--- RTOSv6/app/appAbst.c
+++ RTOSv6/app/appAbst.c
@@ -1,9 +1,20 @@
 //インテリジェンス持たせる
 
+void vdg_appAbstif_mainloop(void);
+static vds_appAbst_proceeded(void);
+static vds_appAbst_discrete(void);
+
+
 void vdg_appAbstif_mainloop(void)
 {
-
+    vds_appAbst_proceeded();
+    vds_appAbst_discrete();
 }
+
+
+
+
+
 
 う～ン，どういう構造にすればいいかね
 メインからコールされるのが連続と非連続のやつ，？

15d385c569d5a3bbf0fd0dec6b3265ca17180e74
WhiteRiceMGMG <3223mq118@gmail.com>
2025-09-15 16:23:39 +0900

    a

--- RTOSv6/app/appAbst.c
+++ RTOSv6/app/appAbst.c
@@ -23,7 +23,30 @@ static void vds_appAbst_discrete(void)
 
 }
 
+static _Bool vds_kedigsw_pushed(void)
+{
+    return vdg_inputAbstif_igswPush();
+}
 
+
+
+
+_Bool vdg_inputAbstif_igswPush(void)
+{
+    return checkEvent(INPUT_BUTTON_IGSW);
+}
+_Bool vdg_inputAbstif_drvswupPush(void)
+{
+    return checkEvent(INPUT_BUTTON_DRVSWUP);
+}
+_Bool vdg_inputAbstif_drvswdwPush(void)
+{
+    return checkEvent(INPUT_BUTTON_DRVSWDW);
+}
+_Bool vdg_inputAbstif_confswPush(void)
+{
+    return checkEvent(INPUT_BUTTON_CONFSW);
+}
 
 
 

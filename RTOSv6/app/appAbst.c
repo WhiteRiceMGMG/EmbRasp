3d43b3f426d013456b906aa74ceab419a62ddfce
WhiteRiceMGMG <3223mq118@gmail.com>
2025-09-15 15:44:59 +0900

    a

--- RTOSv6/app/appAbst.c
+++ RTOSv6/app/appAbst.c
@@ -1,8 +1,8 @@
 //インテリジェンス持たせる
 
 void vdg_appAbstif_mainloop(void);
-static vds_appAbst_proceeded(void);
-static vds_appAbst_discrete(void);
+static void vds_appAbst_proceeded(void);
+static void vds_appAbst_discrete(void);
 
 
 void vdg_appAbstif_mainloop(void)
@@ -11,7 +11,17 @@ void vdg_appAbstif_mainloop(void)
     vds_appAbst_discrete();
 }
 
+/*連続系処理*/
+static void vds_appAbst_proceeded(void)
+{
+    
+}
 
+/*離散系処理*/
+static void vds_appAbst_discrete(void)
+{
+
+}
 
 
 

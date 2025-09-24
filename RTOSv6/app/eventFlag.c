8a7f099874db343ae87127b783a8a1653ddbda38
ogawa-vaio <3223mq118@gmail.com>
2025-09-24 21:56:55 +0900

    a

--- RTOSv6/app/eventFlag.c
+++ RTOSv6/app/eventFlag.c
@@ -8,18 +8,21 @@
 static EventFlag_t inputEventFlag;
 static EventFlag_t outputEventFlag;
 
-void initialEventFlag(void)
+void
+initialEventFlag(void)
 {
     inputEventFlag.flags = 0;
     outputEventFlag.flags  = 0;
 }
 
-void setEventFlag(EventFlag_t *ef, u16 event)
+void
+setEventFlag(EventFlag_t *ef, u16 event)
 {
     ef->flags |= event;
 }
 
-_Bool checkEventFlag(EventFlag_t *ef, u16 event)
+_Bool
+checkEventFlag(EventFlag_t *ef, u16 event)
 {
     if(eventFlag.flags & event)
     {
@@ -29,7 +32,8 @@ _Bool checkEventFlag(EventFlag_t *ef, u16 event)
     return false;
 }
 
-_Bool peekEventFlag(EventFlag_t *ef, u16 event)
+_Bool
+peekEventFlag(EventFlag_t *ef, u16 event)
 {
     return (ef->flags & event) != 0;
 }

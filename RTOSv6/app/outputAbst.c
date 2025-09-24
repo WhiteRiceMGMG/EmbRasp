6bb8b650d28d3f39326fbe459bfe65d9a793e14e
ogawa-vaio <3223mq118@gmail.com>
2025-09-24 21:54:27 +0900

    a

--- RTOSv6/app/outputAbst.c
+++ RTOSv6/app/outputAbst.c
@@ -3,23 +3,26 @@
 // あいうえお
 
 
-
-void vdg_outputAbstif_igledon(void)
+void
+vdg_outputAbstif_igledon(void)
 {
-
+    setEventFlag(OUTPUT_EVENT_FLAG, OUTPUT_LED_IG);
 }
 
-void vdg_outputAbstif_drvupledon(void)
+void
+vdg_outputAbstif_drvupledon(void)
 {
-
+    setEventFlag(OUTPUT_EVENT_FLAG, OUTPUT_LED_DRVUP);
 }
 
-void vdg_outputAbstif_drvdwledon(void)
+void
+vdg_outputAbstif_drvdwledon(void)
 {
-
+    setEventFlag(OUTPUT_EVENT_FLAG, OUTPUT_LED_DRVDW);
 }
 
-void vdg_outputAbstif_confledon(void)
+void
+vdg_outputAbstif_confledon(void)
 {
-
+    setEventFlag(OUTPUT_EVEMT_FLAG, OUTPUT_LED_CONF);
 }
